e75d81c23d77be299777b82e2dc79868e5a7a7a2
Philipp Borkovic <102319879+pborkovic@users.noreply.github.com>
2024-09-20 10:58:21 +0200

    ADDED custom navigation script

---
+++ custom_navigation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sew() {
+    cd "/Users/pb/Documents/other/Developer/sew"
+    echo "Navigated to SEW directory"
+}
+
+insy() {
+    cd "/Users/pb/Documents/other/Developer/mariadb_setup"
+    echo "Navigated to INSY directory"
+}
+
+web() {
+    cd "/Users/pb/Documents/other/Developer/web-wordpress"
+    echo "Navigated to WEB directory"
+}
+
+echo "Custom navigation functions loaded successfully!"
