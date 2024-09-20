29f9baf90c842f4b97819f135672a3ee513f28e5
Philipp Borkovic <102319879+pborkovic@users.noreply.github.com>
2024-09-20 08:55:37 +0200

    ADDED Docker aliases

---
+++ docker_aliases.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Alias for 'docker compose up -d'
+alias dc='docker compose up -d'
+
+# Alias for 'docker compose down'
+alias dcd='docker compose down'
+
+# Alias for 'docker ps'
+alias dps='docker ps'
+
+# Alias for 'docker images'
+alias dpi='docker images'
+
+echo "Docker aliases loaded successfully!"
